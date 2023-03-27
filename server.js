3ae2c19eef63bd3c8427e9d83d2ef7e5397b4347
Sanjay <ksanjayk@protonmail.com>
2023-03-27 13:16:55 +0530

    not found handler added

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import 'express-async-errors';
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import notFound from './middleware/notFound';
+
 const { PORT } = process.env;
 
 const app = express();
@@ -9,6 +11,9 @@ app.use(express.json());
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
+
+app.use(notFound);
+
 const port = PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
